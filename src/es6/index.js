0af8891947c43bce6235382d7377c62ced8f11e1
Emmanuel García <dimple.pleod@gmail.com>
2020-04-02 12:42:11 -0600

    Arrow Functions, Promesas y Parámetros en objetos

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,5 +1,36 @@
 
 // ---> Variables
+//antes
+function oldFunction(name, age, country) {
+    name = name || 'Emmanuel'
+    age = age || 28
+    country = country || 'México'
+    
+    console.log(name, age, country)
+}
+
+//es6
+function nowFunction(name='Emmanuel', age=28, country='Mexico'){
+    console.log(name, age, country)
+}
+
+nowFunction()
+nowFunction('Lisa', 38, 'Germany')
+
+// ---> templates literals
+//antes
+let hello = 'Hola'
+let world = 'World'
+let epicPhrase = hello + " " + world
+console.log(epicPhrase)
+
+//es6
+epicPhrase2 = `${hello} ${world}`
+console.log(epicPhrase2)
+
+
+
+// ---> Variables
 function oldFunction(name, age, country) {
     name = name || 'Emmanuel'
     age = age || 28
