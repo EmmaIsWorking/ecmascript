14d64020d0b05b4506b9bc3af6a71ff383b3ff6c
Emmanuel García <dimple.pleod@gmail.com>
2020-04-02 17:02:03 -0600

    awaiy & trycatch

--- src/es8/index.js
+++ src/es8/index.js
@@ -31,3 +31,36 @@ const msn = 'Feliz'
 console.log(msn.padStart(11, 'Estoy '));
 
 console.log(msn.padEnd(11, ' estoy'))
+
+
+// --->await
+
+const promesaEjemplo = () =>{
+    return new Promise((resolve, reject) =>{
+        (true)
+            ? setTimeout (() => resolve('aceptado'), 3000)
+            : reject(new Error('error'))
+    })
+}
+
+const promesaAsync = async () => {
+    const promesa = await promesaEjemplo()
+    console.log(promesa);
+} 
+
+promesaAsync()
+
+// --->Trycatch
+
+const promesaAsync2 = async () =>{
+    try {
+        const promesa = await promesaEjemplo()
+        console.log(promesa);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+promesaAsync2()
