3aa6d8d051a3215e0a5ee5cfa66bd562aa00a2a7
Emmanuel García <dimple.pleod@gmail.com>
2020-04-02 11:15:23 -0600

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,6 +1,5 @@
 
 // ---> Variables
-//antes
 function oldFunction(name, age, country) {
     name = name || 'Emmanuel'
     age = age || 28
@@ -17,8 +16,10 @@ function nowFunction(name='Emmanuel', age=28, country='Mexico'){
 nowFunction()
 nowFunction('Lisa', 38, 'Germany')
 
+
+
 // ---> templates literals
-//antes
+
 let hello = 'Hola'
 let world = 'World'
 let epicPhrase = hello + " " + world
@@ -28,3 +29,67 @@ console.log(epicPhrase)
 epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
 
+
+
+//---> Multilinea
+
+let dantePhrase = "¡Oh vosotros los que entráis \n" 
++ "abandonad toda esperanza!"
+
+//es6
+let lutherPhrase = `"Desde muy joven decidí dar mi vida por algo eterno y absoluto.
+No por los pequeños dioses que están hoy aquí y mañana se han ido, 
+sino por Dios que es el mismo ayer, hoy y siempre"
+`
+
+console.log(dantePhrase)
+console.log(lutherPhrase)
+
+
+
+//---> Destructuración
+
+let person = {
+    'name': 'Emmanuel',
+    'age': 28,
+    'country': 'Mexico'
+}
+
+console.log(person.name, person.age, person.country)
+
+//es6
+
+let {name, age, country} = person
+
+console.log(name, age, country)
+
+
+
+// --->Spread Operator
+
+let team1 = ['Diana', 'Paola', 'Paco']
+let team2= ['Lisa', 'Judith', 'Petter']
+
+let family= ['Angelica', 'Emmanuel', ...team1, ...team2]
+
+console.log(family)
+
+//LET & CONST
+
+{
+
+    var globalVar = "Global Var"
+}
+
+{
+    let globalLet = 'Global let' 
+    //Solo se puede acceder dentro del codigo.
+    console.log(globalLet)
+
+}
+
+console.log(globalVar)
+
+const a = 'b'
+a = 'a'
+//ERROR
